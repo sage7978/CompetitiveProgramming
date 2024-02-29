dce6ea1019709254aa7884111ca774d25b87a43d
sage7978 <44961824+sage7978@users.noreply.github.com>
2024-02-29 12:28:30 +0530

    Create NFACTOR.java

---
+++ SPOJ/NFACTOR.java
@@ -0,0 +1,196 @@
+// Problem: N-Factorful
+// Contest: SPOJ - Classical
+// URL: https://www.spoj.com/problems/NFACTOR/
+// Memory Limit: 1536 MB
+// Time Limit: 2484 ms
+// Edited By - Satish Kumar Patra
+// 
+// Powered by CP Editor (https://cpeditor.org)
+// status - accepted, linear sieve, prefix
+
+import java.io.*;
+import java.util.*;
+import java.math.*;
+public class Main {
+    InputStream is;
+    PrintWriter out;
+    String INPUT = "";
+    
+    static final int N = 1000000 + 1;
+    //class  Declaration
+	
+	
+	void solve() throws Exception {
+		int[] lp = new int[N];
+		int[] dpr = new int[N];
+		int[] distinct = new int[N];
+		int[][] prefix = new int[N][31];
+		int ds = 0;
+		for(int i=2;i<N;i++){
+			if(lp[i] == 0){
+				lp[i] = i;
+				dpr[ds++] = i;
+				distinct[i]++;
+				prefix[i][distinct[i]]++;
+			}
+			for(int j=0; j<ds && dpr[j]*i <N;j++){
+				lp[i*dpr[j]] = dpr[j];
+				int num = i*dpr[j];
+				int p = dpr[j];
+				distinct[i*dpr[j]] = 1;
+				while(num%dpr[j] == 0){
+					num /= dpr[j];
+				}
+				if(i * dpr[j] != num && num!=1){
+					distinct[i*dpr[j]] = 1 + distinct[num];
+				}
+				prefix[i*dpr[j]][distinct[i*dpr[j]]]++; 	
+				if(dpr[j] == lp[i]){
+					break;
+				}
+			}
+		}
+		
+		// for(int i=0;i<10;i++){
+			// dbg(dpr[i]);
+		// }	
+// 		
+		// for(int i=1;i<N;i++){
+			// distinct[i] += distinct[i-1];
+		// }
+
+		
+		// for(int i=1;i<N;i++){
+			// for(int j=1;j<=20;j++){
+				// prefix[i][j] += prefix[i-1][j];
+			// }
+		// }
+		
+		// for(int i=1;i<N;i++){
+			// String s = i + " 	";
+			// for(int j=1;j<=30;j++){
+				// s += prefix[i][j] + " ";
+			// }
+			// dbg(s);
+		// }
+// 		
+		// dbg("*************************");
+		prefix[1][0]++;
+		for(int i=1;i<N;i++){
+			for(int j=0;j<=20;j++){
+				prefix[i][j] += prefix[i-1][j];
+			}
+		}
+		
+		// for(int i=1;i<N;i++){
+			// String s = i + " 	";
+			// for(int j=1;j<=30;j++){
+				// s += prefix[i][j] + " ";
+			// }
+			// dbg(s);
+		// }
+		
+		int t = ni();
+		while(t-- > 0){
+			int l = ni();
+			int r = ni();
+			int d = ni();
+			pn(prefix[r][d] - prefix[l-1][d]);
+		}
+	}
+	
+	void run() throws Exception {
+	        is = INPUT.isEmpty() ? System.in : new ByteArrayInputStream(INPUT.getBytes());
+	        out = new PrintWriter(System.out);
+	 
+	        long s = System.currentTimeMillis();
+	        solve();
+	        out.flush();
+	        System.err.println(System.currentTimeMillis() - s + "ms");
+	}
+	public static void main(String[] args) throws Exception {
+	    boolean memory = false ;
+	    if(memory) new Thread(null, new Runnable() {public void run(){try{new Main().run();}catch(Exception e){e.printStackTrace();System.exit(1);}}}, "1", 1 << 28).start();
+	    else new Main().run();
+	    
+	}
+	 
+	//output methods
+	private void dbg(Object... o){ System.err.println(Arrays.deepToString(o));}
+	void p(Object... o){for(Object oo:o)out.print(oo);}
+	void pn(Object... o){for(int i = 0; i< o.length; i++)out.print(o[i]+(i+1 < o.length?" ":"\n"));}
+	void pni(Object... o){for(Object oo:o)out.print(oo+" ");out.println();out.flush();}
+	 
+	//input methods
+	
+	private byte[] inbuf = new byte[1024];
+	public int lenbuf = 0, ptrbuf = 0;
+	 
+	 
+	private int readByte() {
+	    if (lenbuf == -1) throw new InputMismatchException();
+	    if (ptrbuf >= lenbuf) {
+	        ptrbuf = 0;
+	        try {
+	            lenbuf = is.read(inbuf);
+	        } catch (IOException e) {
+	            throw new InputMismatchException();
+	        }
+	        if (lenbuf <= 0) return -1;
+	    }
+	    return inbuf[ptrbuf++];
+	}
+	private boolean isSpaceChar(int c) {
+	    return !(c >= 33 && c <= 126);
+	}
+	private int skip() {
+	    int b;
+	    while ((b = readByte()) != -1 && isSpaceChar(b));
+	    return b;
+	}
+	 
+	private int[] na(int n) {
+	    int[] a = new int[n];
+	    for (int i = 0; i < n; i++) a[i] = ni();
+	    return a;
+	}
+	 
+	private int ni() {
+	    int num = 0, b;
+	    boolean minus = false;
+	    while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'));
+	    if (b == '-') {
+	        minus = true;
+	        b = readByte();
+	    }
+	 
+	    while (true) {
+	        if (b >= '0' && b <= '9') {
+	            num = num * 10 + (b - '0');
+	        } else {
+	            return minus ? -num : num;
+	        }
+	        b = readByte();
+	    }
+	}
+	
+	private long nl() {
+	    long num = 0;
+	    int b;
+	    boolean minus = false;
+	    while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'));
+	    if (b == '-') {
+	        minus = true;
+	        b = readByte();
+	    }
+	 
+	    while (true) {
+	        if (b >= '0' && b <= '9') {
+	            num = num * 10 + (b - '0');
+	        } else {
+	            return minus ? -num : num;
+	        }
+	        b = readByte();
+	    }
+	}
+}
